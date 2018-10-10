6b5fea5ff285835b5ff228e60309f10c082e34dd
jb5595 <jacobberman1995@gmail.com>
2018-10-10 13:44:09 -0400

    Done.

--- src/oo_index.js
+++ src/oo_index.js
@@ -1,4 +1,23 @@
 
 document.addEventListener("DOMContentLoaded", () => {
-  const taskList = new TaskList();
+  const list = document.querySelector("#tasks")
+  const taskList = new TaskList(list);
+  let taskCount = 1;
+
+
+  submit.addEventListener("click", function(e){
+    let userInput;
+  	e.preventDefault()
+    userInput = document.getElementById('new-task-description').value
+  	let task = new Task(userInput);
+    console.log(task.id)
+    taskList.postTask(task, taskCount)
+    document.getElementById(`delete-${taskCount}`).addEventListener("click", function(f){
+      list.removeChild(this.parentElement)
+    });
+    taskCount++;
+
+  });
+
+
 });
